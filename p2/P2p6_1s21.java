68b6a3d0b6adbaf394f59fcc3141cd7092061e26
Isaacmoralesacosta <144178794+Isaacmoralesacosta@users.noreply.github.com>
2023-10-16 00:04:10 -0600

    Add files via upload

---
+++ p2/P2p6_1s21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class P2p6_1s21{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		char opc;
+		int a=1;
+		do{
+			System.out.println("aqui nos repetimos");
+			a++;
+		}while(a<10);
+		a=10;
+		do{
+			System.out.println("acaso se ejecutara esta linea");
+		}while(a<10);
+		do{
+			System.out.println("presiona s para salir: ");
+			opc = teclado.next().charAt(0);
+		} while(opc != 'S' & opc != 's');
+	}
+}
