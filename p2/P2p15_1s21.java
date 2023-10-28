228013ae721718cad3d744bd9ad3adc2eaa63909
Isaacmoralesacosta <144178794+Isaacmoralesacosta@users.noreply.github.com>
2023-10-27 19:34:24 -0600

    Add files via upload

--- p2/P2p15_1s21.java
+++ p2/P2p15_1s21.java
@@ -1,49 +1,58 @@
 import java.util.Scanner;
+
 public class P2p15_1s21{
 	public static void main(String[] args) {
-		Scanner = new Scanner(System.in);
-		int [][] Ma,Mb,Mc;
-		int fa,ca,fb,cb,f,c,t;
-		System.out.println("ingresa el numero de filas para la Matriz A");
-		fa=teclado.nextInt();
-		System.out.println("ingresa el numero de columnas para la Matriz A");
-		ca=teclado.nextInt();
-		System.out.println("ingresa el numero de filas para la Matriz B");
-		fb=teclado.nextInt();
-		System.out.println("ingresa el numero de columnas para la Matriz A");
-		cb=teclado.nextInt();
+		Scanner teclado = new  Scanner(System.in);
+		int[][]Ma,Mb,Mc;
+		int fa,ca,fb,cb,f,c,r=0,h;
+		System.out.println("Ingresa el numero de filas para la Matriz A");
+		fa = teclado.nextInt();
+		System.out.println("Ingresa el numero de colunas para la Matriz A");
+		ca = teclado.nextInt();
+		System.out.println("Ingresa el numero de filas para la Matriz B");
+		fb = teclado.nextInt();
+		System.out.println("Ingresa el numero de colunas para la Matriz B");
+		cb = teclado.nextInt();
 		if (ca == fb){
-			Ma = int[fa][ca];
-			Mb = int[fb][cb];
-			Mc = int[fa][cb];
-			for (f=0; f<fa; f++){
-				for (c=0; c<ca; c++){
-					System.out.println("ingresa el valor para Ma["+f+"]["+c+"]: ");
-					Ma[f][c] = teclado.nextInt();
-				}
+			Ma = new int[fa][fa];
+			Mb = new int[fb][cb];
+			Mc = new int[fa][cb];
+			for(f=0;f<fa;f++){
+				for (c=0;c<ca ;c++ ){
+				System.out.println("Ingresa el valor para Ma["+f+"]["+c+"]: ");
+				Ma[f][c]=teclado.nextInt();
+			}
+		}
+		for(f=0;f<fa;f++){
+			for(c=0;c<ca;c++){
+				System.out.println("Ingresa el valor para Mb["+f+"]["+c+"]: ");
+				Mb[f][c]=teclado.nextInt();
 
 			}
-			for (f=0; f<fb; f++){
-			for (c=0; c<cb; c++){
-					System.out.println("ingresa el valor para Ma["+f+"]["+c+"]: ");
-					Mb[f][c] = teclado.nextInt();
-				}
-
+		}
+        //aqui empesamos la multiplicacion
+        for (f=0;f<fa;f++){
+        	for (c=0;c<cb;c++){
+        		for (h=0;h<fb;h++){
+        			r+=Ma[f][h]*Mb[h][c];
+        			System.out.print("a["+f+"]["+h+"]("+Ma[f][h]+") * ");
+        			System.out.println("b["+h+"]["+c+"]("+Mb[h][c]+")");
+        			
+        		}
+        		System.out.println("---------------");
+        		Mc[f][c]=r;
+        		r=0;
+        	}
+        	
+        }
+        for(f=0;f<fa;f++){
+			for(c=0;c<cb;c++){
+				System.out.println("el resultado para Mc["+f+"]["+c+"]: "+Mc[f][c]);
 			}
-			//aqui empezamos a la multiplicacion
-			for (f=0; f<fa; f++){
-				for (c=0; c<cb; c++){
-					for (t=0; t<fb; t++){
-						r=Ma[f][t]*Mb[c][t] {
-							
-						}]
-					}
-					Mc[f][c]=r;
-
-
+		}
 
 		}else{
-			System.out.println("no se pude realizar el proceso de la multiplicacion");
+			System.out.println("No se puede ralizar el proceso de multiplicacion");
 		}
 	}
 }
