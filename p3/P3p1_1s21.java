8dec63c58b6770fcfeb00ff73149fc03a9dbd83d
Isaacmoralesacosta <144178794+Isaacmoralesacosta@users.noreply.github.com>
2023-11-14 14:37:08 -0600

    Add files via upload

--- p3/P3p1_1s21.java
+++ p3/P3p1_1s21.java
@@ -19,22 +19,45 @@ public class P3p1_1s21{
 		int c=a-b;
 		System.out.println("la resta fue " +c);
 	}
+	 public static void Multiplicacion(){
+		System.out.println("Multiplicacion de dos numeros");
+		System.out.println("ingresa el primer numero");
+		int a=teclado.nextInt();
+		System.out.println("ingresa el segundo numero");
+		int b=teclado.nextInt();
+		int c=a*b;
+		System.out.println("la Multiplicacion fue " +c);
+	}
+	public static void Division(){
+		System.out.println("Division de dos numeros");
+		System.out.println("ingresa el primer numero");
+		int a=teclado.nextInt();
+		System.out.println("ingresa el segundo numero");
+		int b=teclado.nextInt();
+		int c=a/b;
+		System.out.println("la Division fue " +c);
+	}
 		public static void menu(){
 			System.out.println("operaciones basicas");
 			System.out.println("\nA) Suma");
 			System.out.println("B) Resta");
+			System.out.println("\nC) Multiplicacion");
+			System.out.println("D) Division");
+
 			System.out.println("\nque opcion deseas");
+			char opc;
 			do{
-				char opc;
 				System.out.println("presione s para salir:");
-				opc=teclado.next().charAT(0);
-			} while(opc != 'S' != 's');
+				opc=teclado.next().charAt(0);
+			} while(opc != 'S' && opc != 's');
 			}
 		public static void main(String[] args) {
 			menu();
 			String opc=teclado.nextLine();
 			if (opc.equals("A"))Suma();
 			if (opc.equals("B"))Resta();
+			if (opc.equals("C"))Multiplicacion();
+			if (opc.equals("D"))Division();
 		}
 	}
 	
