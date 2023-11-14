243f8b891b3e23a4b6c7a90a908b59e452e41443
Isaacmoralesacosta <144178794+Isaacmoralesacosta@users.noreply.github.com>
2023-11-14 15:48:07 -0600

    Add files via upload

--- p3/P3p2_1s21.java
+++ p3/P3p2_1s21.java
@@ -1,61 +1,167 @@
 import java.util.Scanner;
-public class P3p2_1s21{
-	static Scanner teclado = new Scanner(System.in);
-	public static void Suma(){
-		System.out.println("Suma de dos numeros");
-		System.out.println("ingresa el primer numero");
-		int a=teclado.nextInt();
-		System.out.println("ingresa el segundo numero");
-		int b=teclado.nextInt();
-		int c=a+b;
-		System.out.println("la suma fue" +c);
-	}
-     public static void Resta(){
-		System.out.println("Resta de dos numeros");
-		System.out.println("ingresa el primer numero");
-		int a=teclado.nextInt();
-		System.out.println("ingresa el segundo numero");
-		int b=teclado.nextInt();
-		int c=a-b;
-		System.out.println("la resta fue " +c);
-	}
-	 public static void Multiplicacion(){
-		System.out.println("Multiplicacion de dos numeros");
-		System.out.println("ingresa el primer numero");
-		int a=teclado.nextInt();
-		System.out.println("ingresa el segundo numero");
-		int b=teclado.nextInt();
-		int c=a*b;
-		System.out.println("la Multiplicacion fue " +c);
-	}
-	public static void Division(){
-		System.out.println("Division de dos numeros");
-		System.out.println("ingresa el primer numero");
-		int a=teclado.nextInt();
-		System.out.println("ingresa el segundo numero");
-		int b=teclado.nextInt();
-		int c=a/b;
-		System.out.println("la Division fue " +c);
-	}
-		public static void menu(){
-			System.out.println("operaciones basicas");
-			}
-			do{
-			System.out.println("\nA) Suma");
-			System.out.println("B) Resta");
-			System.out.println("\nC) Multiplicacion");
-			System.out.println("D) Division");
-			System.out.println("\nque opcion deseas");
-			System.out.println("\npresione s para salir");
-		}while(opc != 'S' & opc != 's');
-		public static void main(String[] args) {
-			menu();
-			String opc=teclado.nextLine();
-			if (opc.equals("A"))Suma();
-			if (opc.equals("B"))Resta();
-			if (opc.equals("C"))Multiplicacion();
-			if (opc.equals("D"))Division();
-			if(opc.equals("s")) System.exit(0);
-		}
-	}
+public class P2p5_1s21 {
+    static Scanner teclado = new Scanner(System.in);
+    public static void Suma() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a+b;
+        System.out.println("la suma fue" + c);
+    }
+    public static void Resta() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a-b;
+        System.out.println("la suma fue" + c);   
+    }
+    public static void Multiplicacion() {System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a*b;
+        System.out.println("la suma fue" + c);   
+    }
+
+    public static void Division() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a/b;
+        System.out.println("la suma fue" + c);   
+    }
+
+    public static void menu() {
+        System.out.println("Operaciones básicas");
+    }
+    public static void main(String[] args) {
+        char opc;
+        do {
+            menu();
+            System.out.println("\nA) Suma");
+            System.out.println("B) Resta");
+            System.out.println("C) Multiplicacion");
+            System.out.println("D) Division");
+            System.out.println("\n¿Qué opción deseas?");
+            System.out.println("\nPresiona 's' para salir");
+            opc = teclado.next().charAt(0);
+            switch (opc) {
+                case 'A':
+                import java.util.Scanner;
+
+public class P2p5_1s21 {
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void Suma() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a+b;
+        System.out.println("la suma fue" + c);
+    }
+
+    public static void Resta() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a-b;
+        System.out.println("la suma fue" + c);
+        
+    }
+
+    public static void Multiplicacion() {System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a*b;
+        System.out.println("la suma fue" + c);
+        
+    }
+
+    public static void Division() {
+        System.out.println("Suma de dos numeros");
+        System.out.println("ingresa el primer numero");
+        int a=teclado.nextInt();
+        System.out.println("ingresa el segundo numero");
+        int b=teclado.nextInt();
+        int c=a/b;
+        System.out.println("la suma fue" + c);
+        
+    }
+
+    public static void menu() {
+        System.out.println("Operaciones básicas");
+    }
+
+    public static void main(String[] args) {
+        char opc;
+        do {
+            menu();
+            System.out.println("\nA) Suma");
+            System.out.println("B) Resta");
+            System.out.println("C) Multiplicacion");
+            System.out.println("D) Division");
+            System.out.println("\n¿Qué opción deseas?");
+            System.out.println("\nPresiona 's' para salir");
+
+            opc = teclado.next().charAt(0);
+
+            switch (opc) {
+                case 'A':
+                case 'a':
+                    Suma();
+                    break;
+                case 'B':
+                    Resta();
+                    break;
+                case 'C':
+                    Multiplicacion();
+                    break;
+                case 'D':
+                    Division();
+                    break;
+                case 's':
+                case 'S':
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+        } while (opc != 'S' && opc != 's');
+    }
+}	
+                    Suma();
+                    break;
+                case 'B':
+                    Resta();
+                    break;
+                case 'C':
+                    Multiplicacion();
+                    break;
+                case 'D':
+                    Division();
+                    break;
+                case 's':
+                case 'S':
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opción no válida. Inténtalo de nuevo.");
+            }
+        } while (opc != 'S' && opc != 's');
+    }
+}
+
 	
